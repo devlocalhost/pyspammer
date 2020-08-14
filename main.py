f9fcbdbf56eafd42178a9945d3d591a99e39b632
devlocalhost <devlocalhost@github.com>
2020-08-14 16:55:19 +0300

    main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+import time
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+print("  Welcome to the python spammer!!")
+print("  Make sure you open a app quickly\n  and focus your mouse on the text box,\n  therwise bad things could happen!!!")
+
+tx = input("  Enter the text that you want to spam: ")
+
+nm = input('  Enter the amount of times that you want to spam "{}": '.format(tx))
+
+# spd = input("  Enter the speed that the script should spam the text (example: \n  0.01 seconds, 1 second ,3 ,5): ")
+
+print("  Starting....")
+time.sleep(1)
+print("  PLEASE FOCUS ON A APPS\n  TEXTBOX!!! YOU HAVE 5 SECONDS!!")
+
+time.sleep(1)
+print("  4")
+time.sleep(1)
+print("  3")
+time.sleep(1)
+print("  2")
+time.sleep(1)
+print("  1")
+time.sleep(1)
+print('  Spamming "{}" {} times...'.format(tx, nm))
+time.sleep(1)
+
+for num in range(int(nm)):
+    for letter in tx:
+        keyboard.press(letter)
+        keyboard.release(letter)
+    keyboard.press(Key.enter)
+    keyboard.release(Key.enter)
+    time.sleep(0.000000000000000000000000000000000000000000000000001)
+    # time.sleep(int(spd))
+
+print("  Done")
